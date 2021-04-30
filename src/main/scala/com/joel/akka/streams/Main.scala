17b8474ff2507a1b7b5962c8ea601f03c2dd6577
Johannes Ehm <johannes.ehm@gmail.com>
2021-04-30 22:57:08 +0200

    Extends the logging example

--- src/main/scala/com/joel/akka/streams/Main.scala
+++ src/main/scala/com/joel/akka/streams/Main.scala
@@ -1,22 +1,49 @@
 package com.joel.akka.streams
 
 import akka.actor.ActorSystem
-import akka.stream.scaladsl.{Flow, Sink, Source}
+import akka.stream.KillSwitches
+import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
 
-object Main extends App {
+import scala.concurrent.Await
+import scala.concurrent.duration.DurationInt
 
-  implicit val system: ActorSystem = ActorSystem("QuickStart")
+object Main {
 
-  val flow = Flow[Int]
-    .map(1 / _) // throwing ArithmeticException: / by zero
-    //.log("error logging") // only this log statement leads to an exception log statement
-    .to(Sink.ignore)
+  def main(args: Array[String]): Unit = {
 
-  Source(-5 to 5)
-    //.wireTap(flow)
-    .divertTo(flow, _ => true)
-    .map(_ + 1)
-    .log("error logging")
-    .runForeach(println)
+    implicit val system: ActorSystem = ActorSystem("QuickStart")
+
+    val anotherFlow = Flow[Int]
+      .async // exception boundary
+      .map({ bla =>
+        1 / 0
+        "bla"
+      }) // throwing ArithmeticException: / by zero
+      .log("error logging") // only this log statement leads to an exception log statement
+      .to(Sink.ignore)
+
+    val flow = Flow[Int]
+      .divertTo(anotherFlow, _ => true)
+      .log("error logging")
+      .to(Sink.ignore)
+
+    val (killSwitch, future) = Source(-5 to 5)
+      //.wireTap(flow) // exception boundary
+      .divertTo(flow, _ => true)
+      .map(_ + 1)
+      .log("error logging")
+      .viaMat(KillSwitches.single)(Keep.right)
+      .toMat(Sink.ignore)(Keep.both) // materializes the stream but only keeps the future
+      .run()
+    //.runForeach(println)
+    //.runWith(Sink.foreach(println))
+
+    Await.result(future, 1.second)
+
+    killSwitch.shutdown()
+
+    system.terminate()
+
+  }
 
 }
